28766bebf0ceea1c444b664d4b6aa0d3dd46f513
Lior Borbashov <liorborbashov@gmail.com>
2025-05-18 20:51:25 +0300

    added sort function in UserUtils class

--- src/il/ac/hit/validation/UserUtils.java
+++ src/il/ac/hit/validation/UserUtils.java
@@ -1,4 +1,10 @@
 package il.ac.hit.validation;
 
+import java.util.Comparator;
+import java.util.Arrays;
+
 public class UserUtils {
+    public static void sort(User[] users, Comparator<User> userComparator) {
+        Arrays.sort(users, userComparator);
+    }
 }
