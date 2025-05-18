ecd253a5d0c4d22f191f3aea8636e7d470891f06
Lior Borbashov <liorborbashov@gmail.com>
2025-05-18 21:22:43 +0300

    make the variables private and added getter functions

--- src/il/ac/hit/validation/User.java
+++ src/il/ac/hit/validation/User.java
@@ -1,15 +1,40 @@
 package il.ac.hit.validation;
 
 public class User {
-    public String username;
-    public String email;
-    public String password;
-    public int age;
+    private final String username;
+    private final String email;
+    private final String password;
+    private final int age;
 
     public User(String username, String email, String password, int age) {
         this.username = username;
         this.email = email;
         this.password = password;
         this.age = age;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", age=" + age +
+                '}';
     }
 }
