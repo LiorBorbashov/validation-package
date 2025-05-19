e25774373ea546790e028b26ff4246bf80bea925
Lior Borbashov <liorborbashov@gmail.com>
2025-05-19 12:33:04 +0300

    added constructors for Valid/Invalid classes

--- src/il/ac/hit/validation/Invalid.java
+++ src/il/ac/hit/validation/Invalid.java
@@ -3,6 +3,12 @@ package il.ac.hit.validation;
 import java.util.Optional;
 
 public class Invalid implements ValidationResult{
+    private final String reason;
+
+    public Invalid(String reason) {
+        this.reason = reason;
+    }
+
     @Override
     public boolean isValid() {
         return false;
@@ -10,6 +16,6 @@ public class Invalid implements ValidationResult{
 
     @Override
     public Optional<String> getReason() {
-        return Optional.of("invalid reason");
+        return Optional.of(reason);
     }
 }
--- src/il/ac/hit/validation/Valid.java
+++ src/il/ac/hit/validation/Valid.java
@@ -3,6 +3,9 @@ package il.ac.hit.validation;
 import java.util.Optional;
 
 public class Valid implements ValidationResult{
+
+    public Valid() {}
+
     @Override
     public boolean isValid() {
         return true;
